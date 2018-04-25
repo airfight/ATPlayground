6f1be9eb480da09ed1bc83f0554966455beb1750
zhuguangyang <guangyang.zhu@quncaotech.com>
2018-04-25 15:31:32 +0800

    按位和运算 |=

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -4,3 +4,20 @@ import UIKit
 
 var str = "Hello, playground"
 print(str)
+
+enum Status: UInt32 {
+    case disconnect = 0x1
+    case connecting = 0x2
+    case connected = 0x4
+}
+
+func UInt32Value(_ from:[Status]) -> UInt32 {
+    
+    var value: UInt32 = 0
+    for statusItem in from {
+        value |= statusItem.rawValue
+    }
+    return value
+}
+print(UInt32Value([Status.connected,Status.disconnect]))
+
