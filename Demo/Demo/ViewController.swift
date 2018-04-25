a3d1a1b71c5a6befc52963c73deff17e6d2ab2a4
zhuguangyang <guangyang.zhu@quncaotech.com>
2018-04-25 15:47:52 +0800

    |=

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -12,14 +12,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        print(UInt32Value([Status.connected,Status.connected,Status.connecting,Status.connecting,Status.disconnect]))
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    enum Status: UInt32 {
+        case disconnect = 0x1
+        case connecting = 0x2
+        case connected = 0x4
+    }
+    
+    func UInt32Value(_ from:[Status]) -> UInt32 {
+        
+        var value: UInt32 = 0
+        for statusItem in from {
+            value |= statusItem.rawValue
+        }
+        return value
+    }
 
 }
 
